3bfeb69f6a62e674f6f25419d9d5d45cc58d9e0d
MathieuNico <mathieu.nicolas49@gmail.com>
2025-06-04 09:43:20 +0200

    Change techno to vueJs

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { authGuard } from '../router/authGuard.js'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+
+    {
+      path: '/login',
+      name: 'login',
+      
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue'),
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/ProductsView.vue'),
+    }
+  ],
+})
+
+router.beforeEach(authGuard)
+
+export default router
