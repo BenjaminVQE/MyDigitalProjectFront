07dde288c914d99077f5bb070d5aeab452f9e3b0
MathieuNico <mathieu.nicolas49@gmail.com>
2025-06-05 15:18:42 +0200

    Add product and carts

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -17,15 +17,22 @@
 
 <template>
     <div>
-    <h2>Liste des produits</h2>
-    <v-list>
-      <v-list-item
-        v-for="product in products"
-        :key="product.id"
-      >
-        <v-list-item-title>{{ product.name }}</v-list-item-title>
-        <v-list-item-subtitle>{{ product.matter }}</v-list-item-subtitle>
-      </v-list-item>
-    </v-list>
+    <h1 style="text-align: center;">Liste des produits</h1>
+    <v-container>
+      <v-row>
+        <v-col
+        v-for="product in products.member" 
+        :key="product.id" cols="12" sm="6" md="4">
+          <v-card class="pa-2">
+            <v-card-title class="text-h6">{{ product.name }}</v-card-title>
+            <v-card-subtitle>{{ product.price }} €</v-card-subtitle>
+            <v-card-actions>  
+              <v-btn color="primary" @click="cart.addToCart(product)">Ajouter au panier</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+    </v-row>
+
+    </v-container>
   </div>
 </template>
